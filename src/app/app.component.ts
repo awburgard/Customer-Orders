5cb670186ebfe5644c473a6ae0496a82ef639d63
Austin Burgard <awburgard@gmail.com>
2019-07-27 10:54:20 -0500

    adds customers component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+  `
 })
-export class AppComponent {
-  title = 'customer-orders';
+export class AppComponent implements OnInit {
+  title: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Hello World!';
+
+  }
 }

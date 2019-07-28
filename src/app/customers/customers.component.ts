4f8f04acae1ce6ba212c8f9ee6afc9c001d86907
Austin Burgard <awburgard@gmail.com>
2019-07-28 12:55:33 -0500

    adds comment

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ICustomer } from '../shared/interfaces';
 
+// Creates Customers
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
